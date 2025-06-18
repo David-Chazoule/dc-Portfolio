b421ad5af08bd0b0987f92ed0fb36becb83dec88
David-Chazoule <d.chazoule@gmail.com>
2025-06-18 19:32:35 +0200

    modification structure footer

--- DC-portfolio/src/components/footer/Footer.tsx
+++ DC-portfolio/src/components/footer/Footer.tsx
@@ -1,5 +1,16 @@
 function Footer() {
-  return <div className="footer-container">Footer</div>;
+  return (
+    <div className="footer-container">
+      <h1>By David</h1>
+      <div className="nav">
+        <h2>A propos</h2>
+        <h2>Portfolio</h2>
+      </div>
+      <div className="copyRight">
+        <p>David.com. All Rights reserved</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
