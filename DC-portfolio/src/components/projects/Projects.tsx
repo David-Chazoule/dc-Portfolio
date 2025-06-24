1e8fb46542605aeb215cb2bd6762b623ca4c7316
David-Chazoule <d.chazoule@gmail.com>
2025-06-24 19:51:46 +0200

    structure Projects component + importation image

--- DC-portfolio/src/components/projects/Projects.tsx
+++ DC-portfolio/src/components/projects/Projects.tsx
@@ -5,7 +5,13 @@ import { LanguageContext } from "../../context/LanguageContext";
 function Projects() {
   const { theme } = useContext(ThemeContext);
   const { language } = useContext(LanguageContext);
-  return <div className={`projects-container ${theme}`}>Projects</div>;
+  return (
+    <div className={`projects-container ${theme}`}>
+      <h3>Projet</h3>
+      <h1>Mon Portfolio</h1>
+      <div className="projects-wrap"></div>
+    </div>
+  );
 }
 
 export default Projects;
