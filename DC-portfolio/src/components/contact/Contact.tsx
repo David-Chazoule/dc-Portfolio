7e5596570a99d33c94fb7f3c2a549927ab70d274
David-Chazoule <d.chazoule@gmail.com>
2025-06-26 18:27:30 +0200

    code pour envoi de mail, structure base sass pour le composant contact

--- DC-portfolio/src/components/contact/Contact.tsx
+++ DC-portfolio/src/components/contact/Contact.tsx
@@ -1,31 +1,61 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import { LanguageContext } from "../../context/LanguageContext";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef<HTMLFormElement>(null);
   const { theme } = useContext(ThemeContext);
   const { language } = useContext(LanguageContext);
 
-  return <div className={`contact-container ${theme}`}>
-  <h1>Contactez moi</h1>
-  <div className="contact-card">
-   <form>
-    <input type="text" name="user_name" placeholder="votre nom"/>
-    <input type="email" name="user_email" placeholder="votre adresse mail"/>
-   <textarea name="message" placeholder="votre message"></textarea>
-   <button type="submit">Envoyez</button>
+  const sendEmail = (e) => {
+    e.preventDefault();
+    if (!form.current) return;
+    emailjs
+      .sendForm(
+        "service_wt9aw4k",
+        "template_0mh4nso",
+        form.current,
+        "t8uciT4BW5C9SZHxh"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
 
-
-   </form>
-
-
-  </div>
-
-
-
-
-
-  </div>;
+  return (
+    <div className={`contact-container ${theme}`}>
+      <h1>Contactez moi</h1>
+      <div className="contact-card">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="user_name"
+            placeholder="votre nom"
+            required
+          />
+          <input
+            type="email"
+            name="user_email"
+            placeholder="votre adresse mail"
+            required
+          />
+          <textarea
+            name="message"
+            placeholder="votre message"
+            required
+          ></textarea>
+          <button type="submit">Envoyez</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
