fcfcb82f7e82e216474ad752c39096695e69b9f8
David-Chazoule <d.chazoule@gmail.com>
2025-06-26 18:14:15 +0200

    structure de base composant contact

--- DC-portfolio/src/components/contact/Contact.tsx
+++ DC-portfolio/src/components/contact/Contact.tsx
@@ -6,7 +6,26 @@ function Contact() {
   const { theme } = useContext(ThemeContext);
   const { language } = useContext(LanguageContext);
 
-  return <div className={`contact-container ${theme}`}>Contact</div>;
+  return <div className={`contact-container ${theme}`}>
+  <h1>Contactez moi</h1>
+  <div className="contact-card">
+   <form>
+    <input type="text" name="user_name" placeholder="votre nom"/>
+    <input type="email" name="user_email" placeholder="votre adresse mail"/>
+   <textarea name="message" placeholder="votre message"></textarea>
+   <button type="submit">Envoyez</button>
+
+
+   </form>
+
+
+  </div>
+
+
+
+
+
+  </div>;
 }
 
 export default Contact;
