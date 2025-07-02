4a094b0f737cc72a476d3c8faf0a10662176023e
David-Chazoule <d.chazoule@gmail.com>
2025-07-02 21:11:20 +0200

    structure de base de introduction componant

--- DC-portfolio/src/components/introduction/Introduction.tsx
+++ DC-portfolio/src/components/introduction/Introduction.tsx
@@ -1,13 +1,34 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import { LanguageContext } from "../../context/LanguageContext";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { BsMouse } from "react-icons/bs";
 
 function Introduction() {
   const { theme } = useContext(ThemeContext);
   const { language } = useContext(LanguageContext);
   return (
-    <div className={`introduction-container ${theme}`}>Introduction</div>
-  )
+    <div className={`introduction-container ${theme}`}>
+      <h3>Hello, je suis</h3>
+      <h1>David</h1>
+      <h3>Développeur web </h3>
+      <div className="cv-about-container">
+        <button>Télécharger le cv</button>
+        <button>A propos de moi</button>
+      </div>
+      <div className="container">
+        <div className="left-box">
+          <FaLinkedin />
+          <FaGithub />
+        </div>
+        <div className="middle"></div>
+        <div className="right-box">
+          <BsMouse />
+          <span>Scroll Down</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Introduction
+export default Introduction;
