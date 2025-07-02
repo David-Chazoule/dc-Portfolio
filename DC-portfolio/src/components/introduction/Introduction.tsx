1a05c31d71c86b6dff62cb1fb5e45afdfb1f718e
David-Chazoule <d.chazoule@gmail.com>
2025-07-02 21:28:10 +0200

    ajout boutton + scrolling,modification structure

--- DC-portfolio/src/components/introduction/Introduction.tsx
+++ DC-portfolio/src/components/introduction/Introduction.tsx
@@ -3,27 +3,37 @@ import { ThemeContext } from "../../context/ThemeContext";
 import { LanguageContext } from "../../context/LanguageContext";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { BsMouse } from "react-icons/bs";
+import photo from "../about/img/profilePicture.jpg";
 
 function Introduction() {
   const { theme } = useContext(ThemeContext);
   const { language } = useContext(LanguageContext);
+
+  const scrollTo = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className={`introduction-container ${theme}`}>
-      <h3>Hello, je suis</h3>
+      <h3>{language==='fr'? 'Hello, je suis':"Hello, i'am"}</h3>
       <h1>David</h1>
       <h3>Développeur web </h3>
       <div className="cv-about-container">
         <button>Télécharger le cv</button>
-        <button>A propos de moi</button>
+        <button onClick={() => scrollTo("about")}>A propos de moi</button>
       </div>
-      <div className="container">
+      <div className="container-img">
         <div className="left-box">
-          <FaLinkedin />
-          <FaGithub />
+          <FaLinkedin className="icon-social" />
+          <FaGithub className="icon-social" />
         </div>
-        <div className="middle"></div>
-        <div className="right-box">
-          <BsMouse />
+        <div className="middle-box">
+          <img src={photo} alt="" />
+        </div>
+        <div className="right-box" onClick={() => scrollTo("about")}>
+          <BsMouse className="icon-mousse" />
           <span>Scroll Down</span>
         </div>
       </div>
