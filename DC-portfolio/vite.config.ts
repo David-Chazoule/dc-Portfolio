a6b4f4e7420648bd6b30e8caaa8e961cbaec8b74
David-Chazoule <d.chazoule@gmail.com>
2025-10-29 17:14:51 +0100

    modification responsive pour mobile, reparation couleur

--- DC-portfolio/vite.config.ts
+++ DC-portfolio/vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // <-- permet d’écouter sur l’IP locale
+    port: 5173  // ou ton port actuel
+  }
   
 })
