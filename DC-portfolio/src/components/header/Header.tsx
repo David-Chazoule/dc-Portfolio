d06c06a51a1bd060917a4e4ac61ae665a8707128
David-Chazoule <d.chazoule@gmail.com>
2025-10-30 19:50:28 +0100

    intégration logo

--- DC-portfolio/src/components/header/Header.tsx
+++ DC-portfolio/src/components/header/Header.tsx
@@ -2,7 +2,8 @@ import { useContext, useState, useEffect } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 import ButtonTheme from "../UI/buttonTheme/ButtonTheme";
 import ButtonTranslate from "../UI/buttonTranslate/ButtonTranslate";
-
+import dark from "./img/logoDark.png";
+import light from "./img/logoLight.png";
 function Header() {
   const { theme } = useContext(ThemeContext);
   const [scrolled, setScrolled] = useState(false);
@@ -13,9 +14,19 @@ function Header() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
   return (
-    <div className={`header-container ${theme} ${scrolled ? `scrolled-${theme}` : ""} `}>
+    <div
+      className={`header-container ${theme} ${
+        scrolled ? `scrolled-${theme}` : ""
+      } `}
+    >
       <div className="header-box">
-        <div className="logo">Logo</div>
+        <div className="logo">
+          {theme === "light" ? (
+            <img src={light} alt="" />
+          ) : (
+            <img src={dark} alt="" />
+          )}
+        </div>
         <div className="settings">
           <ButtonTranslate />
           <ButtonTheme />
