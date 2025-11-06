61664a9f0396198231c2faedabb8989057fd57d1
David-Chazoule <d.chazoule@gmail.com>
2025-11-06 19:57:21 +0100

    modification nom domaine footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -52,7 +52,7 @@ function Footer() {
         transition={{ ease: "easeOut", duration: 2 }}
       >
         <p className={theme === "light" ? "" : "pDark"}>
-          portfolio.davidc.vercel.app/ All Rights reserved
+          dc-portfolio-one.vercel.app/ All Rights reserved
         </p>
       </motion.div>
     </div>
